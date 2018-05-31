728ce5444b1dfcfaa7e67be4deb53ae3e63b0d7d
ashishkumarsinghh <ashishkumarsinghh@outlook.com>
2018-05-31 23:02:27 +0530

    created popular tweet finding logic

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require("express")
 const Twit = require("twit")
-const sgMail = require('@sendgrid/mail');
-
+const sgMail = require('@sendgrid/mail')
+const fs = require("fs")
 
 const app = express()
 
 const T = new Twit({
-  consumer_key: process.env.TWITTER_API_KEY,
-  consumer_secret: process.env.TWITTER_API_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_API_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 })
 
 
-let count = 0;
-
-app.get("/", function(req, res){
-  res.send("A simple twitter bot by Ashish Kumar Singh.")
+app.get("/", function(req, res) {
+    fs.readFile("PopTweets.txt", function(err, buf) {
+        if (err)
+            res.send(err)
+        else
+            res.send(buf.toString());
+    })
 })
 
 // 
@@ -26,51 +29,38 @@ app.get("/", function(req, res){
 // })
 
 
-app.get("/tracklist", function(req, res){
-  let result = "";
-  for(let i=0;i<trackList.length;i++){
-    result+=trackList[i]+"  ";
-  }
-  res.send(result);
-})
+T.get("lists/statuses", { owner_screen_name: "ks_ashi", slug: "javascript" }, function(error, data) {
+    //console.log(data)
+    if (data.text)
+        fs.appendFile("PopTweets.txt", data.text, function(err, content) {
+            if (err)
+                console.log(err)
+            else {
+                //console.log(content);
+            }
+        });
+});
 
-app.get("/count", function(req, res){
-  res.send("Total tweets :"+count)
-})
 
-let trackList = []
-
-trackList.push("mongodb")
-trackList.push("es6")
-trackList.push("javascript")
-
-let stream = T.stream('statuses/filter', { track: trackList })
-stream.on("tweet", function(tweet){
-  count++;
-  //console.log(tweet.text)
-})
-
-function sendMail(){
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'warrior2602@gmail.com',
-  from: 'ashwebdeveloper@gmail.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail.send(msg);
+function sendMail() {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'warrior2602@gmail.com',
+        from: 'ashwebdeveloper@gmail.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    sgMail.send(msg);
 }
 
 
-function addToTrackList(item){
-  trackList.push(item);
-  stream = T.stream('statuses/filter', { track: trackList })
-  stream.on("tweet", function(tweet){
-    count++;
-    //console.log(tweet.text)
-  })
+function reset() {
+    fs.writeFile("PopTweets.txt", "", function(err, data) {
+        if (err)
+            console.log(err);
+    });
 }
 
 
-app.listen(process.env.PORT, ()=> console.log("server running on."+process.env.PORT));
+app.listen(process.env.PORT, () => console.log("server running on." + process.env.PORT));
